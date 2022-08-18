ed0a949935b598246a09c6ebdf2dd73451c3da3f
Damian <damian.e.ferrara@gmail.com>
2022-08-18 00:23:02 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Mascotas Felices</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            
+            <NavDropdown title="Comprar" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Alimentos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Accesorios y Juguetes</NavDropdown.Item>
+                          
+            </NavDropdown>
+            <Nav.Link href="#link">Contacto</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
